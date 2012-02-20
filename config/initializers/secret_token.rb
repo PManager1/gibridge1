20460a7e14c3d8797351d0b73467e6195d71f51b
jaipratik <jp_ca@live.com>
2012-02-20 03:07:44 -0800

    second

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bridge1::Application.config.secret_token = '3e7eb40032ba058eecaffb4be7bd5bb2ec6c19c99563d82cb6462ead90c9b30b145f5b4450fe1402ce02f51815981925a18560f829241ccdd979eedf14fd5cff'
